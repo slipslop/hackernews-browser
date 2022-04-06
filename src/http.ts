3473e181ca0479eb0eaf4cfc00a994063cc44f6f
Vili <vili.takaanttila@gmail.com>
2022-04-06 19:25:51 +0300

    Improve typing in http helper

--- src/http.ts
+++ src/http.ts
@@ -2,17 +2,18 @@ const apiBase = 'https://hacker-news.firebaseio.com/v0/';
 
 export interface storyResponse extends ArrayLike<number>{
     id: number,
-    descendants? :number,
-    by?: string,
-    score?: number,
-    time?: number,
-    title?: string,
-    type?: string,
-    url?: string,
+    descendants :number,
+    by: string,
+    score: number,
+    time: number,
+    title: string,
+    type: string,
+    url: string,
+    text?: string,
     slice(start?: number, end?: number): Array<storyResponse>
 }
 
-async function http(url: string): Promise<storyResponse> {
+async function http<T>(url: string): Promise<T> {
     const response = await fetch(url);
 
     if (!response.ok) {
@@ -24,14 +25,18 @@ async function http(url: string): Promise<storyResponse> {
 
 export async function getTopStories() {
     const url = apiBase + 'topstories.json';
-    return await http(url).then((data) => {
+    return await http<Array<number>>(url).then((data) => {
         return data;
     });
 }
 
-export async function getStory(storyId: any) {
+export async function getStory(storyId: string|undefined) {
+    if (storyId === undefined) {
+        throw Error('Story id is undefined');
+    }
+
     const url = apiBase + `item/${storyId}.json`;
-    return await http(url).then((data) => {
+    return await http<storyResponse>(url).then((data) => {
         return data;
     });
 }

0acce1346cb365c6796c405b6e38cf8f0e14a601
Vili <vili.takaanttila@gmail.com>
2022-04-06 19:55:10 +0300

    Style different article types

--- src/Article/Types/CommentArticle.tsx
+++ src/Article/Types/CommentArticle.tsx
@@ -1,13 +1,17 @@
 import { storyResponse } from "../../http";
+import { Link } from "react-router-dom";
 
 export function CommentArticle(props: {article: storyResponse}) {
     let article = props.article;
     return (
         <>
-            <p>{article.title}</p>
-            <p>{article.by}</p>
-            <p>{article.id}</p>
-            <p>{article.text}</p>
+            <p className='title'>{article.title}</p>
+            <p>Comment by: {article.by}</p>
+            <p>id: {article.id}</p>
+            <p className="comment">{article.text}</p>
+            <button className="btn">
+                <Link to={`/`}>Home</Link>
+            </button>
         </>
     );
 }
--- src/Article/Types/StoryArticle.tsx
+++ src/Article/Types/StoryArticle.tsx
@@ -1,13 +1,17 @@
 import { storyResponse } from "../../http";
+import { Link } from "react-router-dom";
 
 export function StroyArticle(props: {article: storyResponse}) {
     let article = props.article;
     return (
         <>
-            <p>{article.title}</p>
-            <p>{article.by}</p>
-            <p>{article.id}</p>
-            <p>{article.url}</p>
+            <p className="title">{article.title}</p>
+            <p>Linked by: {article.by}</p>
+            <p>id: {article.id}</p>
+            <a href={article.url} target="_blank">Link to article</a>
+            <button className="btn">
+                <Link to={`/`}>Home</Link>
+            </button>
         </>
     );
 }

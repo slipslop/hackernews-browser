33b913196632123d3858d4dc8175e390e98f715c
slipslop <vili.takaanttila@gmail.com>
2022-04-06 12:57:09 +0300

    Add routing to single post

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import './index.css';
 import { ArticleListing } from './ArticleListing';
+import { Article } from './Article';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ArticleListing />
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ArticleListing/>}></Route>
+          <Route path="/article/:id" element={<Article/>}></Route>
+        </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );

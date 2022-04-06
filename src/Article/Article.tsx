54eb67adb70a726d2704ecba9e2b08dd599c4c5b
Vili <vili.takaanttila@gmail.com>
2022-04-06 19:27:20 +0300

    Add view of single article

---
+++ src/Article/Article.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
+import { getStory } from "../http";
+import { storyResponse } from "../http";
+import { CommentArticle } from "./Types/CommentArticle";
+import { StroyArticle } from "./Types/StoryArticle";
+
+export function Article(props: any) {
+    let {id} = useParams();
+
+    const [article, setArticle] = useState<storyResponse>();
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    const fetchData = () => {
+        getStory(id).then(response => {
+            return response;
+        })
+        .then(data => {
+            console.log(data);
+            setArticle(data);
+            setIsLoading(false);
+        });
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    if (article && !isLoading) {
+        if (article.type === 'comment') {
+            return <CommentArticle article={article}/>
+        } else {
+            return <StroyArticle article={article} />
+        }
+    }
+    
+    if (!article && !isLoading) {
+        return (
+            <>
+                <p>Article not found!</p>
+            </>
+        );
+    }
+
+    return (
+        <>
+            <p>Loading data...</p>
+        </>
+    );
+}

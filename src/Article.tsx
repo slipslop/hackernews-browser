cc52e6ade72ad1b2b31efead36f9dd7d5256e9f9
slipslop <vili.takaanttila@gmail.com>
2022-04-06 12:57:39 +0300

    Create a view for single post

--- src/Article.tsx
+++ src/Article.tsx
@@ -1,14 +1,30 @@
-import { BrowserRouter, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
+import { getStory } from "./http";
+import { storyResponse } from "./http";
 
 export function Article(props: any) {
-    let id = props.id;
+    let {id} = useParams();
 
-    
+    const [article, setArticle] = useState<storyResponse>();
+
+    const fetchData = () => {
+        getStory(id).then(response => {
+            return response;
+        })
+        .then(data => {
+            console.log(data);
+            setArticle(data);
+        });
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
     return (
-        <div>
-            <BrowserRouter>
-                <Link to={`/${id}`}>{id}</Link>
-            </BrowserRouter>
-        </div>
-    )
+        <>
+            <p>{article?.by}</p>
+        </>
+    );
 }
